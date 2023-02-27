f32d80bc89c7a2d6788c24c52c6f1cb882df41e8
TheVibeKage <marcosmartinez760@gmail.com>
2023-02-26 20:06:52 -0600

    Home Page
    Stylized home page and created a log in and sign up form

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,24 @@
 import min_logo from "../images/logo.png"
-import max_logo from "../images/big_logo.png"
-const Nav = ({minimal, authToken}) => {
+import max_logo from "../images/white_logo.png"
 
-    //const minimal = true
+const Nav = ({minimal, authToken, setShowModal, showModal, setIsSignUp}) => {
+
+    const handleClick = () => {
+        setShowModal(true)
+        setIsSignUp(false)
+    }
+
     return (
         <nav>
             <div className="logo-container">
-                <img className="logo" src = {minimal ? min_logo : max_logo}/>
+                <img className="logo" src = {minimal ? min_logo : max_logo} alt="missing logo"/>
             </div>
 
-            {!authToken && !minimal && <button className="nav-button">Log in</button>}
+            {!authToken && !minimal && <button
+                className="nav-button"
+                onClick={handleClick}
+                disabled={showModal}
+            >Log in</button>}
         </nav>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,39 @@
 import Nav from "../components/Nav"
+import AuthModal from "../components/AuthModal"
+import { useState } from 'react'
 
 const Home = () => {
+    const [showModal, setShowModal] = useState(false)
+    const [isSignUp, setIsSignUp] = useState(true)
 
     const authToken = false
 
     const handleClick = () => {
         console.log('clicked')
+        setShowModal(true)
+        setIsSignUp(true)
     }
     return (
         <div className="overlay">
-        <Nav minimal={false} authToken={authToken}/>
+        <Nav minimal={false}
+            authToken={authToken}
+            setShowModal={setShowModal}
+            showModal={showModal}
+            setIsSignUp={setIsSignUp}
+
+        />
         <div className="home">
-            <h1>Be Yourself</h1>
+            <h1 className="primary-title">Be Yourself</h1>
             <button className="primary-button" onClick={handleClick}>
                 {authToken ? 'Signout' : 'Create Account'}
             </button>
+
+            {showModal && (
+                <AuthModal 
+                setShowModal={setShowModal}
+                isSignUp={isSignUp}
+                />
+            )}
 
         </div>
         </div>
